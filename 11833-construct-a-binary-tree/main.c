a7d2b2284994043a9f71d7caa069e7fac2e7a1d8
duckhanson <ducklyu0301@gmail.com>
2020-03-24 21:05:35 +0800

    specify the output of postorder 11833

--- 11833-construct-a-binary-tree/main.c
+++ 11833-construct-a-binary-tree/main.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #define ONLINE_JUDGE
-typedef struct _node {
-  int val;
-  struct _node *left, *right;
-} Node;
+#define ONLINE_JUDGE
 int n;
 int *inorder;
 int *preorder;
-Node *tree;
-Node *BuildTree(int, int);
-void PostOrder(Node *);
+// Node *tree;
+void PostTree(int, int);
 int main() {
-  Node *root;
 #ifndef ONLINE_JUDGE
   freopen("input.txt", "r", stdin);
 #endif
   scanf(" %d", &n);
-  tree = (Node *)malloc(sizeof(Node) * n);
   inorder = (int *)malloc(sizeof(int) * n);
   preorder = (int *)malloc(sizeof(int) * n);
   int i;
-  for (i = 0; i < n; i++) {
-    scanf(" %d", &inorder[i]);
-  }
-  for (i = 0; i < n; i++) {
-    scanf(" %d", &preorder[i]);
-  }
+  for (i = 0; i < n; i++) scanf(" %d", &inorder[i]);
+  for (i = 0; i < n; i++) scanf(" %d", &preorder[i]);
 #ifndef ONLINE_JUDGE
   printf("%d\n", n);
   for (i = 0; i < n; i++) printf("%d ", inorder[i]);
@@ -34,32 +23,19 @@ int main() {
   for (i = 0; i < n; i++) printf("%d ", preorder[i]);
   printf("\n");
 #endif
-  root = BuildTree(0, n - 1);
-  PostOrder(root);
+  PostTree(0, n - 1);
   printf("\n");
 
   return 0;
 }
-void PostOrder(Node *root) {
-  if (root != NULL) {
-    PostOrder(root->left);
-    PostOrder(root->right);
-    printf("%d ", root->val);
-  }
-}
-int pre_idx,  tree_idx;
-Node *BuildTree(int inorder_start, int inorder_end) {
-  if (inorder_start > inorder_end) return NULL;
-  Node *root = &tree[tree_idx++];
-  root->left = root->right = NULL;
-  root->val = preorder[pre_idx++];
-  if (inorder_start == inorder_end) return root;
-  // find root->val in preorder array.
+int pre_idx;
+void PostTree(int inorder_start, int inorder_end) {
+    if (inorder_start > inorder_end) return;
   int i;
-  for (i = inorder_start; i <= inorder_end; i++) {
-    if (inorder[i] == root->val) break;
-  }
-  root->left = BuildTree(inorder_start, i - 1);
-  root->right = BuildTree(i + 1, inorder_end);
-  return root;
+  for (i = inorder_start; i <= inorder_end; i++)
+    if (inorder[i] == preorder[pre_idx]) break;
+  pre_idx++;
+  PostTree(inorder_start, i - 1);
+  PostTree(i + 1, inorder_end);
+  printf("%d ", inorder[i]);
 }
